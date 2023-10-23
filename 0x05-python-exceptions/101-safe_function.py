8947600558c3324b0a1bf30ed1fc22dccdcb8449
Love <mzeelovegeneral@gmail.com>
2023-10-23 20:34:24 +0100

    excecute function, handle exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,6 +3,6 @@ def safe_function(fct, *args):
     import sys
     try:
         return (fct(*args))
-    except:
+    except Exception:
         sys.stderr.write("Exception: {}\n".format(sys.exc_info()[1]))
         return (None)
