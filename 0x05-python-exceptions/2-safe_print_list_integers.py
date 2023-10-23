6102938b88a06fedad0e1517555298268fe80e58
Love <mzeelovegeneral@gmail.com>
2023-10-23 19:06:37 +0100

    print only intergers in list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             try:
                 print("{:d}".format(my_list[i]), end='')
                 counted += 1
-            except Exception:
+            except(ValueError, TypeError):
                 print()
+    print()
     return (counted)
