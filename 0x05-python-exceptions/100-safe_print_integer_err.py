d285c1feaf40dab257a7d0ecb996e92d4a549a3b
Love <mzeelovegeneral@gmail.com>
2023-10-23 20:19:09 +0100

    print int and err to stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
     except (ValueError, TypeError) as err:
         sys.stderr.write("Exception: {}\n".format(err))
-        return(False)
-    return(True)
+        return (False)
+    return (True)
