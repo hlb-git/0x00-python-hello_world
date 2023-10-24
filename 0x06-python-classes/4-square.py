0287a084ee31650d3f6f916a8e16e71b0689a9bc
Love <mzeelovegeneral@gmail.com>
2023-10-24 18:49:10 +0100

    define setter and getter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" define class Square"""
+
+
+class Square:
+    """class with private size field"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """sets value of __self from outside of the class"""
+        if (type(size) is not int):
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """returns the square of the current area"""
+        return (self.__size**2)
