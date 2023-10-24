4bda0f4bc9ab1407baa93c42f5a91c03b19ff7b8
Love <mzeelovegeneral@gmail.com>
2023-10-24 19:21:48 +0100

    print square with class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" define class Square"""
+
+
+class Square:
+    """class with private size field"""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """sets value of __self from outside of the class"""
+        if (type(value) is not int):
+            raise TypeError('size must be an integer')
+        if (value < 0):
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """returns the square of the current area"""
+        return (self.__size**2)
+
+    def my_print(self):
+        if (self.size):
+            for i in range(self.__size):
+                print("#" * self.size, end='')
+                print()
+        else:
+            print()
