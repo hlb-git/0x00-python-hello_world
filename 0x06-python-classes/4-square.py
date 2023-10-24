61bef3114584edcaa936ba09f68e86afd23308e2
Love <mzeelovegeneral@gmail.com>
2023-10-24 18:52:52 +0100

    define setter and getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,9 +14,9 @@ class Square:
     @size.setter
     def size(self, value):
         """sets value of __self from outside of the class"""
-        if (type(size) is not int):
+        if (type(value) is not int):
             raise TypeError('size must be an integer')
-        if (size < 0):
+        if (value < 0):
             raise ValueError('size must be >= 0')
         self.__size = value
 
