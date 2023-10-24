76c642b9a4ba1f0f00036551c8b331835b445245
Love <mzeelovegeneral@gmail.com>
2023-10-24 18:04:57 +0100

    class square with private field size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """class with private size field"""
     def __init__(self, size=0):
-        if (type(size != int)):
+        if not isinstance(self, int):
             raise TypeError('size must be an integer')
         if (size < 0):
             raise ValueError('size must be >= 0')
