edffa0583eeeb80cc999d626bd9c19f76a0d26be
Love <mzeelovegeneral@gmail.com>
2023-10-30 16:38:19 +0100

    add property to rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,13 +6,12 @@ class Rectangle:
     """width and height attributes"""
 
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
         """gets width"""
-
         return self.__width
 
     @width.setter
@@ -21,7 +20,7 @@ class Rectangle:
 
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        if width < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
@@ -36,6 +35,6 @@ class Rectangle:
         """sets height"""
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        if height < 0:
+        if value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
