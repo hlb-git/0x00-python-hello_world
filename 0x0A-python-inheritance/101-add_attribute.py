df42519eb47f4d5d3d550b027045b9fd512a0eb6
Love <mzeelovegeneral@gmail.com>
2023-11-07 02:55:46 +0100

    set new attribute

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""adds new attribute if possible"""
+
+
+def add_attribute(obj, attr, value):
+    if (hasattr(obj, attr)):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attr, value)
