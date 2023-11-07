04ed1e0e8d28072fad36c33cdff1ec6d2d244fe0
Love <mzeelovegeneral@gmail.com>
2023-11-07 23:02:38 +0100

    load, add, and save python arguements

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,6 +8,12 @@ import sys
 
 save_json = __import__('5-save_to_json_file').save_to_json_file
 load_json = __import__('6-load_from_json_file').load_from_json_file
-del (sys.argv[0])
-save_json(sys.argv, "add_item.json")
-print(load_json("add_item.json"))
+
+try:
+    j_list = load_json("add_item.json")
+except:
+    j_list = []
+
+for i in sys.argv[1:]:
+    j_list.append(i)
+save_json(j_list, "add_item.json")
