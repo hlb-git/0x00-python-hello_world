71cc43499bef6e148358e103831ad465a3866f70
Love <mzeelovegeneral@gmail.com>
2023-11-07 17:47:36 +0100

    gis

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""reads and print file content"""
+
+
+def read_file(filename=""):
+    """print filename data"""
+    if filename:
+        with open(filename, mode="r", encoding="UTF-8") as a_file:
+            for i in a_file:
+                print(i, end="")
+        print()
