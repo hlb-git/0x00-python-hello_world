aa73219570fc164bce015749ee59e4e4b8c6f86c
Love <mzeelovegeneral@gmail.com>
2023-11-07 01:33:47 +0100

    define private attribute

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""empty class"""
+
+
+class BaseGeometry:
+    """ geaometry class"""
+
+    def area(self):
+        """raise an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validate attributes"""
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+"""subclass Rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """computes Rectangle shape"""
+
+    def __init__(self, width, height):
+        if not super().integer_validator("width", width):
+            self.__width = width
+        if not super().integer_validator("height", height):
+            self.__height = height
