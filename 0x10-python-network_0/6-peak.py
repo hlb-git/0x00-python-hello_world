8da6a77412a93d11109babd104f6a67857142b2f
Love <mzeelovegeneral@gmail.com>
2024-01-25 23:32:11 +0100

    fix(algorithm): fix pycodestyle linter error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,11 +7,11 @@ def search(low, high, ints):
     if low == high:
         return ints[high]
     if ints[mid] < ints[mid + 1]:
-        return(search(mid + 1, high, ints))
-    return(search(low, mid, ints))
+        return (search(mid + 1, high, ints))
+    return (search(low, mid, ints))
 
 
 def find_peak(list_of_integers):
     if not list_of_integers:
         return
-    return(search(0, len(list_of_integers) - 1, list_of_integers))
+    return (search(0, len(list_of_integers) - 1, list_of_integers))
