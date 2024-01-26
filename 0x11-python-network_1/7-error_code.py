92f63b152321b2b3aa3b71ce6065157157bc4336
Love <mzeelovegeneral@gmail.com>
2024-01-26 18:01:13 +0100

    fix(requests): change the request type to get

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    response = requests.post(sys.argv[1])
+    response = requests.get(sys.argv[1])
     if response.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
