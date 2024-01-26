8a67e5f97d98c97853a1afd9479bf4184989684f
Love <mzeelovegeneral@gmail.com>
2024-01-26 18:52:38 +0100

    feat(requests): use github api to access user id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""takes varible in json format"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    headers = {'Authentication': password}
+
+    response = requests.get("https://api.github.com/users/{}".format(username),
+                             headers=headers)
+    if response.status_code == 200:
+        id = response.json().get('id')
+        print(id)
+    else:
+        print("None")
