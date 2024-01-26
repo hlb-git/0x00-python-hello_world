8886517905c8fb4b046046fb73604361b0bcb581
Love <mzeelovegeneral@gmail.com>
2024-01-26 17:59:49 +0100

    feat(requests): handle error code from request

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""handle error code from request"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    response = requests.post(sys.argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(response.text)
