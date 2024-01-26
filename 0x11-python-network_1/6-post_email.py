f038f65c805a7cc969ca8cc8183f6f815a536155
Love <mzeelovegeneral@gmail.com>
2024-01-26 17:50:53 +0100

    fix(requests): adjust dictionary data syntax

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,5 +5,5 @@ import sys
 
 
 if __name__ == "__main__":
-    response = requests.get(sys.argv[1], data={'email': sys.argv[2])
+    response = requests.get(sys.argv[1], data={'email': sys.argv[2]})
     print(response.text)
