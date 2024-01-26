d145e3f6810bfbe778a60489710fe557308eb96e
Love <mzeelovegeneral@gmail.com>
2024-01-26 18:17:18 +0100

    fix(requests): fix pycodestyle linter error

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,11 +9,13 @@ if __name__ == "__main__":
         value = argv[1]
     else:
         value = ""
-    response = requests.post("http://0.0.0.0:5000/search_user", data={'q': value})
+    response = requests.post("http://0.0.0.0:5000/search_user",
+                             data={'q': value})
     try:
         if response.json():
-            print("[{}] {}".format(response.json().get('id'), response.json().get('name')))
+            print("[{}] {}".format(response.json().get('id'),
+                                   response.json().get('name')))
         else:
             print("No result")
-    except:
+    except Exception:
         print("Not a valid JSON")
