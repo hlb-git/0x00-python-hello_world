aca80e9cfc28a29de9c32d005713dea1d01d671f
Love <mzeelovegeneral@gmail.com>
2024-01-26 18:14:28 +0100

    feat(requests): post to server and use the json response value

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""takes varible in json format"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        value = argv[1]
+    else:
+        value = ""
+    response = requests.post("http://0.0.0.0:5000/search_user", data={'q': value})
+    try:
+        if response.json():
+            print("[{}] {}".format(response.json().get('id'), response.json().get('name')))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
