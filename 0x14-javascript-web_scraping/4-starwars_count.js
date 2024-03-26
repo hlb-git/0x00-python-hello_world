9df041795f79a903d1225c141cc504f152631b85
Love <mzeelovegeneral@gmail.com>
2024-03-26 22:00:34 +0100

    feat(api): fetch a specific character details

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// script to fetch star wars api data usin request library
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const output = JSON.parse(body);
+    console.log(output.title);
+  }
+});
