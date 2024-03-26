16a5f56619757ec6bba37df4f516b34e55265aba
Love <mzeelovegeneral@gmail.com>
2024-03-26 22:11:41 +0100

    fear(api): get employee  todo task using api

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,4 +1,21 @@
 #!/usr/bin/node
 // script to fetch employee todo list api data usin request library
 
-
+const request = require('request');
+request.get(process.argv[2], { json: true }, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(tasksCompleted);
+});
