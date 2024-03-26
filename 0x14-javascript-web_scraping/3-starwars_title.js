b1d0241b28b38471802e4a8f39d4a9dac9dce493
Love <mzeelovegeneral@gmail.com>
2024-03-26 21:55:35 +0100

    feat(api): use request library to get star wars data

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,7 +7,7 @@ const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 request.get(url, (err, response, body) => {
   if (err) {
     console.log(err);
-    } else {
+  } else {
     const output = JSON.parse(body);
     console.log(output.title);
   }
